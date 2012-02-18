4351e7c481c18547d01201f04ba4e6b2893864ae
Dan O'Huiginn <daniel@ohuiginn.net>
2012-02-18 09:53:58 -0500

    Frontend tweaks to work on server

--- site.py
+++ site.py
@@ -1,4 +1,4 @@
-
+import os
 import tornado.ioloop
 import tornado.web
 import json
@@ -9,7 +9,8 @@ mc = memcache.Client(['127.0.0.1:11211'], debug=1)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        indextxt = open('./site/index.html').read()
+        self.write(indextxt)
 
 class DemoHandler(tornado.web.RequestHandler):
     def get(self):
@@ -22,14 +23,18 @@ class DemoHandler(tornado.web.RequestHandler):
             mc.set(mckey, places)
         locations = entities.placelocations(places)
         output = json.dumps(locations)
+        self.content_type = 'application/json'
+        self.set_header("Content-Type", "application/json") 
         self.write(output)
         #placenames = [x['name'] for x in places]
 
- 
+static_path =  os.path.join(os.path.dirname(__file__), "static")
+
 application = tornado.web.Application([ 
     (r"/", MainHandler),
-    (r"/demo", DemoHandler),
-
+    (r"/code/demo", DemoHandler),
+    (r"/js", tornado.web.StaticFileHandler,
+                 dict(path=static_path+'/js')),
                             ])
 if __name__ == "__main__":
     application.listen(9000)
