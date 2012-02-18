4728a86d0be36c8f900b8f17f6733ec0f50da2b8
Dan O'Huiginn <daniel@ohuiginn.net>
2012-02-18 09:02:36 -0500

    Tornado demo and caching
    [and other stuff]

--- entities.py
+++ entities.py
@@ -12,3 +12,20 @@ def places_from_text(text):
             places.append(item)
     return places
 
+def placelocations(places):
+    output = []
+    for place in places:
+        try:
+            latitude =  place['resolutions'][0]['latitude']
+            longitude =  place['resolutions'][0]['longitude']
+        except KeyError:
+            latitude = None
+            longitude = None
+        output.append({
+            'name' : place['name'],
+            'latitude' : latitude,
+            'longitude' : longitude,
+            })
+    return output
+
+
--- site.py
+++ site.py
@@ -4,15 +4,24 @@ import tornado.web
 import json
 import entities, parliament
 
+import memcache
+mc = memcache.Client(['127.0.0.1:11211'], debug=1)
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
 class DemoHandler(tornado.web.RequestHandler):
     def get(self):
-        text = parliament.mptext()
-        places = entities.places_from_text(text)
-        output = json.dumps(places)
+        mpid = '10251'
+        mckey = 'places_%s' % mpid
+        places = mc.get(mckey)
+        if places is None:
+            text = parliament.mptext()
+            places = entities.places_from_text(text)
+            mc.set(mckey, places)
+        locations = entities.placelocations(places)
+        output = json.dumps(locations)
         self.write(output)
         #placenames = [x['name'] for x in places]
 
