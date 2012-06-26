21c9c860bd924a447c88ec5ffc74cbf0296d2bc8
bmcorser <benmarshallcorser@gmail.com>
2012-06-26 23:48:34 +0000

    Basic views and routes set up

--- MPTrends/mptrends/__init__.py
+++ MPTrends/mptrends/__init__.py
@@ -11,5 +11,8 @@ def main(global_config, **settings):
     """
     config = Configurator(root_factory=root_factory, settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('speaker_route', '/speaker/{speaker_id}*options')
+    config.add_route('speaker_json_route', '/json/speaker/{speaker_id}*options')
     config.scan()
     return config.make_wsgi_app()
--- MPTrends/mptrends/views.py
+++ MPTrends/mptrends/views.py
@@ -1,6 +1,14 @@
 from pyramid.view import view_config
 from .models import MyModel
 
-@view_config(context=MyModel, renderer='templates/mytemplate.pt')
-def my_view(request):
+@view_config(context=MyModel, renderer='templates/home.pt', route_name='home')
+def home(request):
+    return {'project':'MPTrends'}
+
+@view_config(context=MyModel, renderer='templates/json/speaker.pt', route_name='speaker_json_route')
+def speaker_json_view(request):
+    return {'project':'MPTrends'}
+
+@view_config(context=MyModel, renderer='templates/speaker.pt', route_name='speaker_route')
+def speaker_view(request):
     return {'project':'MPTrends'}
