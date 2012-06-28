cba100a29429c9bf3536c35350c9916bd6cf42df
bmcorser <benmarshallcorser@gmail.com>
2012-06-28 21:40:51 +0100

    speeches coming out from TWFY API request

--- MPTrends/mptrends/views.py
+++ MPTrends/mptrends/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 from .models import MPTrends
+import re
 
 @view_config(context=MPTrends, renderer='templates/home.pt', route_name='home')
 def home(request):
@@ -8,9 +9,11 @@ def home(request):
 
 @view_config(context=MPTrends, renderer='json', route_name='graph_json')
 def graph_json(request):
-    #for v in request.matchdict['ids']:
-        #speeches = request.context.mpspeeches(int(v))
+    for mpid in request.matchdict['ids']:
+        speeches = request.context.mpspeeches(int(mpid))['rows']
         #import ipdb; ipdb.set_trace()
+        for row in speeches:
+            print re.search(request.matchdict['search'],row['body'])
     return request.context.mpspeeches(int(request.matchdict['ids'][0]))
 
 @view_config(context=MPTrends, renderer='json', route_name='map_json')
