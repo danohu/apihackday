0bd7570daa485c07c88204f41658aa6694c0c4cc
bmcorser <benmarshallcorser@gmail.com>
2012-07-08 12:43:34 +0000

    Cache is vaguely persistent, interface is vaguely working

--- MPTrends/mptrends/models.py
+++ MPTrends/mptrends/models.py
@@ -3,6 +3,9 @@ from persistent import Persistent
 from twfy import TWFY
 import json
 import re
+from time import time
+from crypt import crypt
+from collections import defaultdict
 from kales import Kales
 
 
@@ -12,19 +15,32 @@ class MPTrends(PersistentMapping):
     def chart(self,search,mpids):
         data = []
         for mpid in mpids:
-            # TODO: check for cached version of this query, 24 hr expiry
-            theywork = TWFY.TWFY('AqHCxnC7THtNEPXRyBAcHUfU')
-            rawtxt = str(theywork.api.getHansard(output = 'js', person = mpid, search = search))
-            text = ''.join(x for x in rawtxt if ord(x)<127)
-            speeches = json.loads(text)['rows']
-            data.append({'mpid':mpid,'speeches':speeches})
+            searchHash = crypt(''.join([search,mpid]),'ok')
+            if self['cache'].get(searchHash) == None or self['cache'][searchHash]['time'] < time()-5184000:
+                print 'chart cache missed :('
+                theywork = TWFY.TWFY('AqHCxnC7THtNEPXRyBAcHUfU')
+                rawtxt = str(theywork.api.getHansard(output = 'js', person = mpid, search = search, num = 1000))
+                text = ''.join(x for x in rawtxt if ord(x)<127)
+                speeches = json.loads(text)['rows']
+                self['cache'][searchHash] = {
+                    'speeches':speeches,
+                    'time':time(),
+                }
+                data.append({
+                    'mpid':mpid,
+                    'speeches':speeches,
+                })
+            else:
+                print 'chart cache hit :)'
+                data.append({'mpid':mpid,'speeches':self['cache'][searchHash]['speeches']})
          
         # This is for Google Charts data structure
 
         firstrow = ['Date']
         dates = []
         for MP in data:
-            firstrow.append(MP['mpid'])
+            speaker = MP['speeches'][0]['speaker']
+            firstrow.append(speaker['first_name']+' '+speaker['last_name'])
             for speech in MP['speeches']:
                 dates.append(speech['hdate'][:-3])
         
@@ -47,11 +63,29 @@ class MPTrends(PersistentMapping):
         return rows
 
     def mplist(self):
-        theywork = TWFY.TWFY('AqHCxnC7THtNEPXRyBAcHUfU')
-        rawtxt = str(theywork.api.getMPs(output = 'js'))
-        text = ''.join(x for x in rawtxt if ord(x)<127)
-        MPs = json.loads(text)
-        return MPs
+        if self.get('cache') == None: self['cache'] = {}
+        if self['cache'].get('mplist') == None or self['cache']['mplist']['time'] < time()-5184000:
+            theywork = TWFY.TWFY('AqHCxnC7THtNEPXRyBAcHUfU')
+            rawtxt = str(theywork.api.getMPs(output = 'js'))
+            text = ''.join(x for x in rawtxt if ord(x)<127)
+            MPs = json.loads(text)
+            MPs.sort(key=lambda MP: MP['name'].split(' ')[1])
+            parties = {}
+            for MP in MPs:
+                if parties.get(MP['party']) == None:
+                    parties[MP['party']] = []
+                    parties[MP['party']].append(MP)
+                else:
+                    parties[MP['party']].append(MP)
+            self['cache']['mplist'] = {
+                'data':parties,
+                'time':time(),
+            }
+            print 'mplist cache missed :('
+            return parties
+        else:
+            print 'mplist cache hit :)'
+            return self['cache']['mplist']['data']
 
 def appmaker(zodb_root):
     if not 'app_root' in zodb_root:
